c23e0f32bab39c7b02e30da82a543ab97254d4c6
Adria Casajus <acasajus@key.cat>
2017-10-17 00:23:51 +0200

    Missing files in the previous commit

--- Makefile
+++ Makefile
@@ -2,8 +2,14 @@ GIT_VERSION = $(shell git describe --abbrev=8 --dirty --always 2>/dev/null)
 
 .PHONY: static
 
-static:
+git-static:
 	mkdir -p data/version
 	git log --pretty=format:'{ "commit": "%H", "date": "%aI"},' | perl -pe 'BEGIN{print "["}; END{print "]\n"}' | perl -pe 's/},]/}]/' > data/version/history
 	echo ${GIT_VERSION} > data/version/current
+
+static: git-static
 	go-bindata -o static/data.go -pkg static data/**
+
+dev-static: git-static
+	go-bindata -debug -o static/data.go -pkg static data/**
+
--- data/migrations/20170101_initial.sql
+++ data/migrations/20170101_initial.sql
@@ -1,33 +1,28 @@
-DROP TABLE IF EXISTS "User";
-CREATE TABLE "User" (
+DROP TABLE IF EXISTS "Users";
+CREATE TABLE "Users" (
 	"Id" STRING NOT NULL,
 	"Email" STRING NOT NULL,
 	"UnconfirmedEmail" STRING NULL,
-	"Password" STRING NOT NULL,
+	"HashPass" BYTEA NOT NULL,
 	"FullName" STRING NOT NULL,
 	"ConfirmedAt" TIMESTAMP WITH TIME ZONE NULL,
 	"LockedAt" TIMESTAMP WITH TIME ZONE NULL,
 	"SignInCount" INT NULL DEFAULT 0,
 	"FailedAttempts" INT NULL DEFAULT 0,
-	"PublicKey" STRING NOT NULL,
-	"Key" STRING NOT NULL,
+	"PublicKey" BYTEA NOT NULL,
+	"Key" BYTEA NOT NULL,
+	"CreatedAt" TIMESTAMP WITH TIME ZONE,
+	"UpdatedAt" TIMESTAMP WITH TIME ZONE,
 	CONSTRAINT "primary" PRIMARY KEY ("Id" ASC),
-	FAMILY "primary" ("Id", "Email", "UnconfirmedEmail", "Password", "FullName", "ConfirmedAt", "LockedAt", "SignInCount", "FailedAttempts", "PublicKey", "Key")
+	FAMILY "primary" ("Id", "Email", "UnconfirmedEmail", "HashPass", "FullName", "ConfirmedAt", "LockedAt", "SignInCount", "FailedAttempts", "PublicKey", "Key", "CreatedAt", "UpdatedAt")
 );
 
-DROP TABLE IF EXISTS "Invites";
-CREATE TABLE "Invites" (
-	"Team" STRING NOT NULL,
-	"Email" STRING NOT NULL,
-	CONSTRAINT "primary" PRIMARY KEY ("Team" ASC),
-	INDEX email ("Email" ASC),
-	FAMILY "primary" ("Team", "Email")
-);
-
-DROP TABLE IF EXISTS "Team";
-CREATE TABLE "Team" (
+DROP TABLE IF EXISTS "Teams";
+CREATE TABLE "Teams" (
 	"Id" STRING NOT NULL,
-	"Owner" STRING NOT NULL,
+	"Name" STRING NOT NULL,
+	"Owner" STRING NOT NULL REFERENCES "Users" ("Id"),
+	"BelongsToUser" BOOL NOT NULL,
 	"Size" INT NOT NULL,
 	"CreatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
 	"UpdatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -35,53 +30,63 @@ CREATE TABLE "Team" (
 	FAMILY "primary" ("Id", "Owner", "Size", "CreatedAt", "UpdatedAt")
 );
 
+DROP TABLE IF EXISTS "Invites";
+CREATE TABLE "Invites" (
+	"Team" STRING NOT NULL REFERENCES "Teams" ("Id"),
+	"Email" STRING NOT NULL,
+	CONSTRAINT "primary" PRIMARY KEY ("Team", "Email"),
+	INDEX email ("Email" ASC),
+	FAMILY "primary" ("Team", "Email")
+) INTERLEAVE IN PARENT "Teams" ("Team");
+
 DROP TABLE IF EXISTS "Team_Users";
 CREATE TABLE "Team_Users" (
-	"Team" STRING NOT NULL,
-	"Username" STRING NOT NULL,
+	"Team" STRING NOT NULL REFERENCES "Teams" ("Id"),
+	"Username" STRING NOT NULL REFERENCES "Users" ("Id"),
 	"Admin" BOOL NOT NULL,
 	"RequiresAccess" BOOL NOT NULL,
-	CONSTRAINT "primary" PRIMARY KEY ("Team", "Username" ASC),
+	CONSTRAINT "primary" PRIMARY KEY ("Team", "Username"),
+	INDEX "team" ("Team"),
 	FAMILY "primary" ("Team", "Username", "Admin", "RequiresAccess")
+) INTERLEAVE IN PARENT "Teams" ("Team");
+
+DROP TABLE IF EXISTS "Tokens";
+CREATE TABLE "Tokens" (
+	"Id" STRING NOT NULL,
+	"Type" INT NOT NULL,
+	"Username" STRING NULL REFERENCES "Users" ("Id"),
+	CONSTRAINT "primary" PRIMARY KEY ("Id" ASC),
+	FAMILY "primary" ("Id", "Type", "Username")
 );
 
-DROP TABLE IF EXISTS "Token";
-CREATE TABLE "Token" (
+DROP TABLE IF EXISTS "Vaults";
+CREATE TABLE "Vaults" (
 	"Id" STRING NOT NULL,
-	type INT NOT NULL,
-	username STRING NULL,
-	CONSTRAINT "primary" PRIMARY KEY ("Id" ASC),
-	CONSTRAINT username FOREIGN KEY (username) REFERENCES "User" ("Id"),
-	FAMILY "primary" ("Id", type, username)
-);
-
-DROP TABLE IF EXISTS "Vault";
-CREATE TABLE "Vault" (
-	"Id" STRING NOT NULL,
-	"Team" STRING NOT NULL,
-	"PublicKey" STRING NOT NULL,
-	CONSTRAINT "primary" PRIMARY KEY ("Id" ASC),
-	CONSTRAINT team FOREIGN KEY ("Team") REFERENCES "Team" ("Id"),
+	"Team" STRING NOT NULL REFERENCES "Teams" ("Id"),
+ 	"PublicKey" STRING NOT NULL,
+	CONSTRAINT "primary" PRIMARY KEY ("Team", "Id" ASC),
 	FAMILY "primary" ("Id", "Team", "PublicKey")
-);
+) INTERLEAVE IN PARENT "Teams" ("Team");
 
 DROP TABLE IF EXISTS "Vault_User";
 CREATE TABLE "Vault_User" (
+	"Team" STRING NOT NULL, 
 	"Vault" STRING NOT NULL,
 	"Username" STRING NOT NULL,
 	"Key" STRING NOT NULL,
-	CONSTRAINT "primary" PRIMARY KEY ("Vault", "Username" ASC),
-	FAMILY "primary" ("Vault", "Username", "Key")
-);
+	CONSTRAINT "primary" PRIMARY KEY ("Team", "Vault", "Username" ASC),
+	FAMILY "primary" ("Team", "Vault", "Username", "Key"),
+	CONSTRAINT "fk_TeamsVaults" FOREIGN KEY ("Team", "Vault" ) REFERENCES "Vaults"
+) INTERLEAVE IN PARENT "Vaults" ("Team", "Vault");
 
-DROP TABLE IF EXISTS "Secret";
-CREATE TABLE "Secret" (
-	"Id" INT NOT NULL DEFAULT unique_rowid(),
-	"Vault" STRING NULL,
+DROP TABLE IF EXISTS "Secrets";
+CREATE TABLE "Secrets" (
+	"Team" STRING NOT NULL,
+	"Vault" STRING NOT NULL,
+	"Id"  STRING NOT NULL,
 	"Data" BYTES NOT NULL,
-	CONSTRAINT "primary" PRIMARY KEY ("Id" ASC),
-	CONSTRAINT "fk_Vault_ref_Vault" FOREIGN KEY ("Vault") REFERENCES "Vault" ("Id"),
-	FAMILY "primary" ("Id", "Vault", "Data")
-);
-
+	CONSTRAINT "primary" PRIMARY KEY ("Team", "Vault", "Id" ASC),
+	FAMILY "primary" ("Team", "Vault", "Id",  "Data"),
+	CONSTRAINT "fk_TeamsVaults" FOREIGN KEY ("Team", "Vault" ) REFERENCES "Vaults"
+) INTERLEAVE IN PARENT "Vaults" ("Team","Vault");
 
