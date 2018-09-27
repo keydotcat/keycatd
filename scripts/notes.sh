728b282353055c9ab2bb4314ce3bbaca843cf866
Adria Casajus <adriancasajus@gmail.com>
2018-09-27 12:30:53 +0200

    Added script to generate release notes

---
+++ scripts/notes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+me=$(realpath $0)
+here=$(dirname $me)
+rootDir=${here}/..
+
+function showChanges() {
+	last=$(git tag | sort -r | head -n 1)
+	prev=$(git tag | sort -r | head -n 2 | tail -n 1)
+	git log ${prev}..${last} '--pretty=format:%s' | grep -i -e '^new:' -e '^fix:' -e '^change:' | sort -r | sed 's/^/* /'
+}
+
+echo "## Backend"
+echo 
+showChanges
+echo 
+
+(
+cd $rootDir/web
+echo "## Web"
+echo 
+showChanges
+)
