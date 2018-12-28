78b408165a817663a226c5c1a65c1671df21a5ef
Adria Casajus <adriancasajus@gmail.com>
2018-12-28 23:21:26 +0100

    Reordered event source code

--- api/eventsource.go
+++ api/eventsource.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/keydotcat/keycatd/util"
 )
@@ -29,6 +30,27 @@ func (ah apiHandler) eventSourceFlusher(w http.ResponseWriter) http.Flusher {
 	return flusher
 }
 
+type eventSourceSender struct {
+	w http.ResponseWriter
+	f http.Flusher
+}
+
+func (e eventSourceSender) sendPing() error {
+	if _, err := fmt.Fprintf(e.w, "id: %d\ndata: {\"action\": \"ping\"}\n\n", time.Now().UTC().UnixNano()); err != nil {
+		return err
+	}
+	e.f.Flush()
+	return nil
+}
+
+func (e eventSourceSender) sengMessage(msg []byte) error {
+	if _, err := fmt.Fprintf(e.w, "id: %d\ndata: %s\n\n", time.Now().UTC().UnixNano(), string(msg)); err != nil {
+		return err
+	}
+	e.f.Flush()
+	return nil
+}
+
 // /eventsource
 func (ah apiHandler) eventSourceSubscribe(w http.ResponseWriter, r *http.Request) error {
 	flusher := ah.eventSourceFlusher(w)
@@ -36,17 +58,6 @@ func (ah apiHandler) eventSourceSubscribe(w http.ResponseWriter, r *http.Request
 		return nil
 	}
 	flusher.Flush()
-	return ah.broadcastEventListenLoop(r, func() error {
-		if _, err := fmt.Fprintf(w, "data: {\"action\": \"ping\"}\n\n"); err != nil {
-			return err
-		}
-		flusher.Flush()
-		return nil
-	}, func(msg []byte) error {
-		if _, err := fmt.Fprintf(w, "data: %s\n\n", string(msg)); err != nil {
-			return err
-		}
-		flusher.Flush()
-		return nil
-	})
+	ess := eventSourceSender{w, flusher}
+	return ah.broadcastEventListenLoop(r, ess.sendPing, ess.sengMessage)
 }
